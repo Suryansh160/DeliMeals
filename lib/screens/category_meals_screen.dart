710834a9a618137da5bb1ec977d994a08e4a986e
Suryansh160 <suryanshgupta160@gmail.com>
2025-02-01 10:48:36 +0530

    added web images

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+import '../widgets/dummy_data.dart';
+
+class CategoryMeals extends StatelessWidget {
+  static const routeName = '/categories';
+  // final String id;
+  // final String title;
+
+  // CategoryMeals(this.id, this.title);
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where(
+      (meal) {
+        return meal.categories.contains(categoryId);
+      },
+    ).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle as String),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+              affordability: categoryMeals[index].affordability,
+              complexity: categoryMeals[index].complexity,
+              duration: categoryMeals[index].duration,
+              imageUrl: categoryMeals[index].imageUrl,
+              title: categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
